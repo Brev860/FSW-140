1ed7ba1e4ef530360c707cd24923c381e022db09
Brev860 <brevan.daniels@bryanuniversity.edu>
2020-10-11 23:48:12 -0400

    week3

---
+++ week1/avengers/superheros.sql
@@ -0,0 +1,40 @@
+CREATE TABLE superheros 
+   (ID INTEGER PRIMARY KEY,
+    alias_name TEXT,
+    popularity INTEGER,
+    alignment TEXT,
+    gender TEXT, 
+    height_m NUMERIC,
+    weight_kg NUMERIC,
+    hometown TEXT
+   );
+    
+INSERT INTO superheros VALUES(1, "Spider Man", 1, "Good", "Male", 1.78, 75.75, "USA"); 
+INSERT INTO superheros VALUES(2, "Iron Man", 20, "Neutral", "Male", 1.98, 102.58, "USA"); 
+INSERT INTO superheros VALUES(3, "Hulk", 18, "Neutral", "Male", 2.44, 635.29, "USA"); 
+INSERT INTO superheros VALUES(4, "Wolverine", 3, "Good", "Male", 1.6, 88.46, "Canada");
+INSERT INTO superheros VALUES(5, "Thor", 5, "Good", "Male", 1.98, 290.3, "Asgard");
+INSERT INTO superheros VALUES(6, "Green Goblin", 91, "Bad", "Male", 1.93, 174.63, "USA");
+INSERT INTO superheros VALUES(7, "Magneto", 11, "Neutral", "Male", 1.88, 86.18, "Germany");
+INSERT INTO superheros VALUES(8, "Thanos", 47, "Bad", "Male", 2.01, 446.79, "Titan");
+INSERT INTO superheros VALUES(9, "Loki", 32, "Bad", "Male", 1.93, 238.14, "Jotunheim");
+INSERT INTO superheros VALUES(10, "Doctor Doom", 19, "Bad", "Male", 2.01, 188.24, "Latveria");
+INSERT INTO superheros VALUES(11, "Jean Grey", 8, "Good", "Female", 1.68, 52.16, "USA");
+INSERT INTO superheros VALUES(12, "Rogue", 4, "Good", "Female", 1.73, 54.43, "USA");
+INSERT INTO superheros VALUES(13, "Storm", 2, "Good", "Female", 1.80, 66, "Kenya");
+INSERT INTO superheros VALUES(14, "Nightcrawler", 6, "Good", "Male", 1.75, 73, "Germany");
+INSERT INTO superheros VALUES(15, "Gambit", 7, "Good", "Male", 1.88, 81, "EUA");
+INSERT INTO superheros VALUES(16, "Captain America", 9, "Good", "Male", 1.88, 108, "EUA");
+INSERT INTO superheros VALUES(17, "Cyclops", 10, "Good", "Male", 1.90, 88, "EUA");
+INSERT INTO superheros VALUES(18, "Emma Frost", 12, "Neutral", "Female", 1.78, 65, "EUA");
+INSERT INTO superheros VALUES(20, "Daredevil", 14, "Good", "Male", 1.83, 91, "EUA");
+INSERT INTO superheros VALUES(21, "Punisher", 50, "Neutral", "Male", 1.85, 91, "EUA");
+INSERT INTO superheros VALUES(22, "Silver Surfer", 33, "Good", "Male", 1.93, 102, "Zenn-La");
+INSERT INTO superheros VALUES(23, "Ghost Rider", 86, "Good", "Male", 1.88, 99, "EUA");
+INSERT INTO superheros VALUES(24, "Venon", 78, "Neutral", "Male", 1.90, 118, "EUA");
+INSERT INTO superheros VALUES(25, "Juggernaut", 76, "Neutral", "Male", 2.87, 862, "EUA");
+INSERT INTO superheros VALUES(26, "BlackMan", 22, "Good", "Male", 1.93, 86, "USA");
+INSERT INTO superheros VALUES(27, "Muscle Man", 34, "Good", "Male", 1.73, 89, "USA");
+INSERT INTO superheros VALUES(28, "ElastaGirl", 48, "Good", "Female", 1.63, 86, "USA");
+INSERT INTO superheros VALUES(29, "Shinobi", 28, "Good", "Male", 1.78, 76, "Italy");
+INSERT INTO superheros VALUES(30, "Kitana", 38, "Good", "Female", 1.95, 96, "USA");
